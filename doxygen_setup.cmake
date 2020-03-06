42c9e28dfdbd9bbf7f5a186b6d0e26ade1ce7517
r-vaillant <r-vaillant@bns-g.com>
2020-03-06 14:36:30 +0900

    Fix bat script, fix camke to only generate Dox when necessary

--- doxygen_setup.cmake
+++ doxygen_setup.cmake
@@ -3,11 +3,41 @@
 if (NOT DOXYGEN_FOUND)
     message(WARNING "Doxygen is needed to build the documentation.")
 else()
-    # this will run doxygen at compilation time
-    add_custom_target(
-        user_documentation ALL
-        ${PROJECT_SOURCE_DIR}/00_INSTRUCTIONS/build_doc.sh
-        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/00_INSTRUCTIONS
-     )
+    # this will run doxygen at compilation time (every single time we run 'make')
+#    add_custom_target(
+#        user_documentation ALL
+#        ${PROJECT_SOURCE_DIR}/00_INSTRUCTIONS/build_doc.sh
+#        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/00_INSTRUCTIONS
+#        COMMENT "Generating API documentation with Doxygen"
+#     )
+
+    # this will run doxygen at compilation time (every single time we run 'make')
+   SET( doxy_html_index_file
+        ${PROJECT_SOURCE_DIR}/00_INSTRUCTIONS/html_english/html/index.html
+        ${PROJECT_SOURCE_DIR}/00_INSTRUCTIONS/html_french/html/index.html
+        )
+
+    file(GLOB_RECURSE
+         dependency_files
+         ${PROJECT_SOURCE_DIR}/00_INSTRUCTIONS/lab_instructions/*.h
+         ${PROJECT_SOURCE_DIR}/src/fileloaders/*.cpp
+         ${PROJECT_SOURCE_DIR}/src/fileloaders/*.h
+         ${PROJECT_SOURCE_DIR}/src/gl_utils/*.cpp
+         ${PROJECT_SOURCE_DIR}/src/gl_utils/*.h
+         ${PROJECT_SOURCE_DIR}/src/qt_gui/*.cpp
+         ${PROJECT_SOURCE_DIR}/src/qt_gui/*.h
+        )
+
+   ADD_CUSTOM_COMMAND(
+     OUTPUT ${doxy_html_index_file}
+     COMMAND ${PROJECT_SOURCE_DIR}/00_INSTRUCTIONS/build_doc.sh
+     WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/00_INSTRUCTIONS
+     MAIN_DEPENDENCY ${PROJECT_SOURCE_DIR}/00_INSTRUCTIONS/doxygen_config.txt
+     DEPENDS ${dependency_files}
+     COMMENT "Generating API documentation with Doxygen"
+   )
+
+   ADD_CUSTOM_TARGET( doc ALL DEPENDS ${doxy_html_index_file} )
+
 
 endif()
--- launch_cmake_visual2017.bat
+++ launch_cmake_visual2017.bat
@@ -1,5 +1,6 @@
 :: call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\amd64\vcvars64.bat"
 
+mkdir build
 cd build
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat" 
 cmake -G "NMake Makefiles JOM" -DCMAKE_BUILD_TYPE=Debug ..
